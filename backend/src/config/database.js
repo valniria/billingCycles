11f7c69ceb09b59d9b5353bc1a4e31fab699f1e0
Valniria <xvalniria@gmail.com>
2017-07-20 13:55:15 -0300

    Conclusion of database and server. Implementation of routes, Cors, errors handlers and schemas of databases. Backend Done.

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,3 +1,11 @@
 const mongoose = require('mongoose')
 mongoose.Promise = global.Promise
 module.exports = mongoose.connect('mongodb://localhost/billingcycles')
+
+mongoose.Error.messages.general.required = "O atributo '{PATH}' é obrigatório."
+mongoose.Error.messages.Number.min = 
+    "O '{VALUE}' informado é menor que o limite mínimo de '{MIN}'."
+mongoose.Error.messages.Number.max = 
+    "O '{VALUE}' informado é maio que o limite mínimo de '{MAX}'."
+mongoose.Error.messages.String.enum = 
+    "O '{VALUE}' não é válido para o atributo '{PATH}'."
